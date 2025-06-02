ef681b203dd0504c24ddcda2e9596b88096fe706
jasnli <167535698+jasnli@users.noreply.github.com>
2025-06-02 01:06:20 -0400

    Update API.java

--- JavaAPIProject/src/main/java/com/example/API.java
+++ JavaAPIProject/src/main/java/com/example/API.java
@@ -7,35 +7,53 @@ import java.io.IOException;
 
 
 public class API {
-    public static String getData(String endpoint) throws Exception {
-        /*endpoint is a url (string) that you get from an API website*/
-        URL url = new URL(endpoint);
-        /*connect to the URL*/
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        /*creates a GET request to the API.. Asking the server to retrieve information for our program*/
-        connection.setRequestMethod("GET");
-        // for APIs like PUBChem, with limited API calls since it is public, this is required
-        connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Java)");
+    public static String getData(String endpoint) {
+        boolean success = false;
+        // retries if there is a 503 error
+        while (!success) {
+            try {
+                /*endpoint is a url (string) that you get from an API website*/
+                URL url = new URL(endpoint);
+                /*connect to the URL*/
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                /*creates a GET request to the API.. Asking the server to retrieve information for our program*/
+                connection.setRequestMethod("GET");
+                // for APIs like PUBChem, with limited API calls since it is public, this is required
+                connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Java)");
 
-        int status = connection.getResponseCode();
-    
-        // 200 means it worked, otherwise it was rejected
-        if (status != 200) {
-            throw new IOException("Server returned status code: " + status + " for URL: " + endpoint);
+                int status = connection.getResponseCode();
+            
+                // 200 means it worked, otherwise it was rejected
+                if (status != 200) {
+                    throw new IOException("Server returned status code: " + status + " for URL: " + endpoint);
+                }
+                /* When you read data from the server, it wil be in bytes, the InputStreamReader will convert it to text. 
+                The BufferedReader wraps the text in a buffer so we can read it line by line*/
+                BufferedReader buff = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;//variable to store text, line by line
+                /*A string builder is similar to a string object but faster for larger strings, 
+                you can concatenate to it and build a larger string. Loop through the buffer 
+                (read line by line). Add it to the stringbuilder */
+                StringBuilder content = new StringBuilder();
+                while ((inputLine = buff.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                success = true;
+                buff.close(); //close the bufferreader
+                connection.disconnect(); //disconnect from server 
+                return content.toString(); //return the content as a string
+            } catch (IOException e) {
+                // TODO: handle exception
+                System.out.println("Server Nonresponsive, wait 1 second!");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                System.out.println("Retrying...");
+            }
         }
-        /* When you read data from the server, it wil be in bytes, the InputStreamReader will convert it to text. 
-        The BufferedReader wraps the text in a buffer so we can read it line by line*/
-        BufferedReader buff = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String inputLine;//variable to store text, line by line
-        /*A string builder is similar to a string object but faster for larger strings, 
-        you can concatenate to it and build a larger string. Loop through the buffer 
-        (read line by line). Add it to the stringbuilder */
-        StringBuilder content = new StringBuilder();
-        while ((inputLine = buff.readLine()) != null) {
-            content.append(inputLine);
-        }
-        buff.close(); //close the bufferreader
-        connection.disconnect(); //disconnect from server 
-        return content.toString(); //return the content as a string
+        return "";      
     }
 }
